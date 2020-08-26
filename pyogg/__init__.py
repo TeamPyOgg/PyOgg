2f0fd0cfec5b90e650b286957969b4be355e0bea
Matthew Walker <mattgwwalker@gmail.com>
2020-08-26 14:23:03 +1200

    Added tests for OpusFile; added comments and error checking to OpusFile

--- pyogg/__init__.py
+++ pyogg/__init__.py
@@ -144,48 +144,100 @@ else:
 if (PYOGG_OGG_AVAIL and PYOGG_OPUS_AVAIL and PYOGG_OPUS_FILE_AVAIL):
     class OpusFile:
         def __init__(self, path):
+            # Open the file
             error = ctypes.c_int()
+            of = opus.op_open_file(
+                ogg.to_char_p(path),
+                ctypes.pointer(error)
+            )
 
-            of = opus.op_open_file(ogg.to_char_p(path), ctypes.pointer(error))
-
+            # Check for errors 
             if error.value != 0:
-                raise PyOggError("file couldn't be opened or doesn't exist. Error code : {}".format(error.value))
+                raise PyOggError(
+                    "File couldn't be opened or doesn't exist. "+
+                    "Error code: {}".format(error.value)
+                )
 
+            # Extract the number of channels in the newly opened file
+            #: Number of channels in audio file.
             self.channels = opus.op_channel_count(of, -1)
 
+            # Allocate sufficient memory to store the entire PCM
             pcm_size = opus.op_pcm_total(of, -1)
+            Buf = opus.opus_int16*(pcm_size*self.channels)
+            buf = Buf()
+            print("len(bytes(buf)):", len(bytes(buf)))
 
-            samples_read = ctypes.c_int(0)
+            # Create a pointer to the newly allocated memory.  It
+            # seems we can only do pointer arithmetic on void
+            # pointers.  See
+            # https://mattgwwalker.wordpress.com/2020/05/30/pointer-manipulation-in-python/
+            buf_ptr = ctypes.cast(
+                ctypes.pointer(buf),
+                ctypes.c_void_p
+            )
 
-            bfarr_t = opus.opus_int16*(pcm_size*self.channels)
+            #: Bytes per sample
+            self.bytes_per_sample = ctypes.sizeof(opus.opus_int16)
 
-            self.buffer = ctypes.cast(ctypes.pointer(bfarr_t()),opus.opus_int16_p)
+            # Read through the entire file, copying the PCM into the
+            # buffer
+            samples = 0
+            while True:
+                # Calculate remaining buffer size
+                # TODO
 
-            ptr = ctypes.cast(ctypes.pointer(self.buffer), ctypes.POINTER(ctypes.c_void_p))
+                # Convert buffer pointer to the desired type
+                ptr = ctypes.cast(
+                    buf_ptr,
+                    ctypes.POINTER(opus.opus_int16)
+                )
+                
+                # Read the next section of PCM
+                ns = opus.op_read(
+                    of,
+                    ptr,
+                    pcm_size*self.channels,
+                    ogg.c_int_p()
+                )
+                
+                # Check for errors
+                if ns<0:
+                    raise PyOggError(
+                        "Error while reading OggOpus file. "+
+                        "Error code: {}".format(error.value)
+                    )
 
-            ptr_init = ptr.contents.value
+                # Increment the pointer
+                buf_ptr.value += (
+                    ns
+                    * self.bytes_per_sample
+                    * self.channels
+                )
 
-            while samples_read.value < pcm_size:
-                ptr.contents.value = ptr_init + samples_read.value*self.channels*2
-                ns = opus.op_read(of, self.buffer , pcm_size*self.channels,ogg.c_int_p())
-                samples_read.value += ns
+                samples += ns
+                
+                # Check if we've finished
+                if ns==0:
+                    break
 
-            ptr.contents.value = ptr_init
-
-            del ptr
-
+            # Close the open file
             opus.op_free(of)
 
-            self.buffer_length = samples_read.value*self.channels*2
-
+            # Opus files are always stored at 48k samples per second
+            #: Number of samples per second (per channel).  Always 48,000.
             self.frequency = 48000
+
+            # Store the buffer as Python bytes
+            #: Raw PCM data from audio file.
+            self.buffer = bytes(buf)
+            
 
         def as_array(self):
             """Returns the buffer as a NumPy array.
 
             The shape of the returned array is in units of (number of
             samples per channel, number of channels).
-
 
             The data type is 16-bit signed integers.
 
@@ -196,19 +248,21 @@ if (PYOGG_OGG_AVAIL and PYOGG_OPUS_AVAIL and PYOGG_OPUS_FILE_AVAIL):
 
             import numpy
 
-            bytes_per_sample = ctypes.sizeof(opus.opus_int16)
-
-            return numpy.ctypeslib.as_array(
+            # Convert the bytes buffer to a NumPy array
+            array = numpy.frombuffer(
                 self.buffer,
-                (self.buffer_length
-                 // bytes_per_sample
+                dtype=numpy.int16
+            )
+
+            # Reshape the array
+            return array.reshape(
+                (len(self.buffer)
+                 // self.bytes_per_sample
                  // self.channels,
                  self.channels)
             )
 
-        def as_bytes(self):
-            bytes(self.buffer)
-
+        
     class OpusFileStream:
         def __init__(self, path):
             """Opens an OggOpus file as a stream.
