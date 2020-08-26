84da579b630ed96b9a82e0e23ff72a340b29b316
Matthew Walker <mattgwwalker@gmail.com>
2020-08-26 12:14:09 +1200

    Revert "Added Sphinx-based documentation"
    This reverts commit acddfaebf0080864ecfbeac900b013b2d7a00143.

--- docs/getting_started.py
+++
@@ -1,9 +0,0 @@
-import numpy
-import pyogg
-
-filename = "left-right-demo-5s.opus"
-opus_file = pyogg.OpusFile(filename)
-
-pcm = opus_file.as_array()
-duration_seconds = pcm.shape[0] / opus_file.frequency
-print("Audio duration (seconds):", duration_seconds)
