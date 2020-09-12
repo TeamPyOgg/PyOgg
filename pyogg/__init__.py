0c21596bfb1924c84f9191f99dcd80e558535386
Matthew Walker <mattgwwalker@gmail.com>
2020-09-12 17:03:08 +1200

    Split VorbisFileStream from __init__.py

--- pyogg/__init__.py
+++ pyogg/__init__.py
@@ -1,12 +1,8 @@
 import ctypes
 
 from .pyogg_error import PyOggError
-
 from .ogg import PYOGG_OGG_AVAIL
-
-from . import vorbis
 from .vorbis import PYOGG_VORBIS_AVAIL, PYOGG_VORBIS_FILE_AVAIL, PYOGG_VORBIS_ENC_AVAIL
-
 from .opus import PYOGG_OPUS_AVAIL, PYOGG_OPUS_FILE_AVAIL, PYOGG_OPUS_ENC_AVAIL
 
 from . import flac
@@ -30,111 +26,10 @@ __version__ = '0.6.14a7'
 PYOGG_STREAM_BUFFER_SIZE = 8192
 
 if (PYOGG_OGG_AVAIL and PYOGG_VORBIS_AVAIL and PYOGG_VORBIS_FILE_AVAIL):
-    # Vorbis File
+    # VorbisFile
     from .vorbis_file import VorbisFile
-    
-    class VorbisFileStream:
-        def __init__(self, path):
-            self.vf = vorbis.OggVorbis_File()
-            error = vorbis.ov_fopen(path, ctypes.byref(self.vf))
-            if error != 0:
-                raise PyOggError("file couldn't be opened or doesn't exist. Error code : {}".format(error))
-                           
-            info = vorbis.ov_info(ctypes.byref(self.vf), -1)
-
-            #: Number of channels in audio file.
-            self.channels = info.contents.channels
-
-            #: Number of samples per second (per channel).  Always
-            #  48,000.
-            self.frequency = info.contents.rate
-
-            array = (ctypes.c_char*(PYOGG_STREAM_BUFFER_SIZE*self.channels))()
-
-            self.buffer_ = ctypes.cast(ctypes.pointer(array), ctypes.c_char_p)
-
-            self.bitstream = ctypes.c_int()
-            self.bitstream_pointer = ctypes.pointer(self.bitstream)
-
-            self.exists = True
-
-            #: Bytes per sample
-            self.bytes_per_sample = 2 # TODO: Where is this defined?
-
-        def __del__(self):
-            if self.exists:
-                vorbis.ov_clear(ctypes.byref(self.vf))
-            self.exists = False
-
-        def clean_up(self):
-            vorbis.ov_clear(ctypes.byref(self.vf))
-
-            self.exists = False
-
-        def get_buffer(self):
-            """get_buffer() -> bytesBuffer, bufferLength
-
-            Returns None when all data has been read from the file.
-
-            """
-            if not self.exists:
-                return None
-            buffer = []
-            total_bytes_written = 0
-            
-            while True:
-                new_bytes = vorbis.ov_read(ctypes.byref(self.vf), self.buffer_, PYOGG_STREAM_BUFFER_SIZE*self.channels - total_bytes_written, 0, 2, 1, self.bitstream_pointer)
-                
-                array_ = ctypes.cast(self.buffer_, ctypes.POINTER(ctypes.c_char*(PYOGG_STREAM_BUFFER_SIZE*self.channels))).contents
-                
-                buffer.append(array_.raw[:new_bytes])
-
-                total_bytes_written += new_bytes
-
-                if new_bytes == 0 or total_bytes_written >= PYOGG_STREAM_BUFFER_SIZE*self.channels:
-                    break
-
-            out_buffer = b"".join(buffer)
-
-            if total_bytes_written == 0:
-                self.clean_up()
-                return(None)
-
-            return out_buffer
-        
-        def get_buffer_as_array(self):
-            """Provides the buffer as a NumPy array.
-
-            Note that the underlying data type is 16-bit signed
-            integers.
-
-            Does not copy the underlying data, so the returned array
-            should either be processed or copied before the next call
-            to get_buffer() or get_buffer_as_array().
-
-            """
-            import numpy
-
-            # Read the next samples from the stream
-            buf = self.get_buffer()
-
-            # Check if we've come to the end of the stream
-            if buf is None:
-                return None
-
-            # Convert the bytes buffer to a NumPy array
-            array = numpy.frombuffer(
-                buf,
-                dtype=numpy.int16
-            )
-
-            # Reshape the array
-            return array.reshape(
-                (len(buf)
-                 // self.bytes_per_sample
-                 // self.channels,
-                 self.channels)
-            )
+    # VorbisFileStream
+    from .vorbis_file_stream import VorbisFileStream
 
 else:
     class VorbisFile:
